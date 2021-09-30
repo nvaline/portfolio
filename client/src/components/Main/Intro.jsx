c60b8d413850b7e70f6b74cd827da493bb02ee23
nick <nickvaline@gmail.com>
2021-09-30 14:26:04 -0700

    working webpack / images loading

--- client/src/components/Main/Intro.jsx
+++ client/src/components/Main/Intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import nick from '../../img/nick_01.jpg';
+import nick from '../../img/nick_01.jpg';
 
 // figure out why the image wont load => check web[ack settings]
 const Intro = () => {
@@ -10,7 +10,7 @@ const Intro = () => {
         Hi, I am <strong>nick valine</strong>
       </h1>
       <p className="section_subtitle section_subtitle-intro">Software Engineer</p>
-      <img src="../../img/nick_01.jpg" alt="a picture of nick" className="intro_img" />
+      <img src={ nick } alt="a picture of nick" className="intro_img" />
     </section>
   );
 };
