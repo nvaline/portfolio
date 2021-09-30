6e5eeb0109cab26daa91e27b494e0f0353742370
nick <nickvaline@gmail.com>
2021-09-30 10:06:42 -0700

    add sections stack/ about me

---
+++ client/src/components/Main/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="about-me" id="About Me">
+      <h2 className="section_title section_title-about">Who I am</h2>
+      <p className="section_subtitle section_subtitle-about">Should be things here adding more words</p>
+      <div className="about_me-body">
+        <p>Going to need to make some shit up to put here about myself... Not sure what I am goiing to write but I will put some of this dumb shit right now to hold space.</p>
+        <p>Going to need to make some shit up to put here about myself... Not sure what I am goiing to write but I will put some of this dumb shit right now to hold space.</p>
+      </div>
+      <img src="" alt="nick close-up" className="about_me-img" />
+    </section>
+  );
+};
+
+export default About;
